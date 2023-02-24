a05a455b3ebdc2473bbbbc2f287186428337a493
noname65 <57306016+noname65@users.noreply.github.com>
2023-02-24 15:47:06 -0500

    Create 4.js

---
+++ Easy/509. Fibonacci Number/4.js
@@ -0,0 +1,7 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    return Math.round(0.4472135954999579*(Math.pow(1.618033988749895,n)-Math.pow(-0.6180339887498949,n)))
+};
